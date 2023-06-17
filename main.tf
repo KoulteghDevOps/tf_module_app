1082b6d192d40930856bd39d1fe4092be8423377
KoulteghDevOps <koultegh@outlook.com>
2023-06-17 21:30:46 +0100

    manifest updated

--- main.tf
+++ main.tf
@@ -60,6 +60,7 @@ resource "aws_autoscaling_group" "asg" {
   min_size            = var.min_size
   vpc_zone_identifier = var.subnet_ids
   target_group_arns   = [aws_lb_target_group.main.arn]
+
   launch_template {
     id      = aws_launch_template.template.id
     version = "$Latest"
@@ -72,6 +73,18 @@ resource "aws_autoscaling_group" "asg" {
         propagate_at_launch = true #tag.value.propagate_at_launch
         value               = tag.value
     }
+  }
+}
+
+resource "aws_autoscaling_policy" "asg-cpu-rule" {
+  autoscaling_group_name = "aws_autoscaling_group.asg.name"
+  name                   = "CPULoadDetect"
+  policy_type            = "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 20.0
   }
 }
 
